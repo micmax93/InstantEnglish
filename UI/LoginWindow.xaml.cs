b1a9632288244aa8bfaba14f8d7557266df8f3ae
Michał El <m.elfartas@gmail.com>
2014-01-30 03:16:21 +0100

    Windows prototypes.

---
+++ UI/LoginWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using ViewModel;
+
+namespace UI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class LoginWindows : Window
+    {
+        public LoginViewModel LoginVM = new LoginViewModel();
+        public LoginWindows()
+        {
+            InitializeComponent();
+            DataContext = LoginVM;
+        }
+
+        private void OnPasswordChanged(object sender, RoutedEventArgs e)
+        {
+            MD5 md5 = new MD5CryptoServiceProvider();
+            var pass = ((PasswordBox) sender).Password;
+            var data = Encoding.Unicode.GetBytes(pass);
+            var hash = md5.ComputeHash(data);
+            LoginVM.Password = Convert.ToBase64String(hash);
+        }
+
+    }
+}
---
+++ ViewModel/LoginViewModel.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+using Microsoft.Expression.Interactivity.Core;
+
+namespace ViewModel
+{
+    public class SimpleCommand : ICommand
+    {
+        public delegate void Exec();
+        public delegate bool Check();
+
+        public SimpleCommand(Exec execFunc)
+        {
+            ExecFunc = execFunc;
+            
+        }
+        public Exec ExecFunc
+        {
+            get;
+            private set;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            ExecFunc.Invoke();
+        }
+
+        public event EventHandler CanExecuteChanged;
+    }
+    public class LoginViewModel
+    {
+        public string Username
+        {
+            get;
+            set;
+        }
+
+        public string Password
+        {
+            get;
+            set;
+        }
+
+        public ICommand DoLogin
+        {
+            get;
+            private set;
+        }
+
+        public ICommand DoRegister
+        {
+            get;
+            private set;
+        }
+
+        bool FormFilled()
+        {
+            return !(String.IsNullOrEmpty(Username)||String.IsNullOrEmpty(Password));
+        }
+
+        void AuthtenticateUser()
+        {
+            if (Password == "ha#lo")
+            {
+                MessageBox.Show("Zalogowano");
+            }
+            else
+            {
+                MessageBox.Show(Password);
+            }
+        }
+
+        void RegsterNewUser()
+        {
+            
+        }
+
+        public LoginViewModel()
+        {
+            DoLogin = new SimpleCommand(AuthtenticateUser);
+            DoRegister = new SimpleCommand(RegsterNewUser);
+        }
+
+    }
+}
