7c3bb0022223a3897a3dc06bd72ef4e370d063ab
Michał El <m.elfartas@gmail.com>
2014-01-31 12:07:49 +0100

    EntryEdit

---
+++ UI/EditEntry.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Input;
+
+namespace UI
+{
+    /// <summary>
+    /// Interaction logic for DictionaryEntry.xaml
+    /// </summary>
+    public partial class EditEntry : Window
+    {
+        public EditEntry()
+        {
+            InitializeComponent();
+        }
+
+        private void LevelInput(object sender, TextCompositionEventArgs e)
+        {
+            if (!String.IsNullOrEmpty(e.Text))
+            {
+                int num;
+                if (!Int32.TryParse(e.Text, out num))
+                {
+                    e.Handled = true;
+                }
+            }
+        }
+    }
+}
